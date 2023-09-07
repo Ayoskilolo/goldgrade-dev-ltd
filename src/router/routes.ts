d48c18b7f48f0c211360a9e8721151e239dbc42e
Ayomide Afolabi <ayoafo82@gmail.com>
2023-09-07 22:17:14 +0100

    Making home page the Landing route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,7 @@ import ContactUsPage from 'src/pages/ContactUsPage.vue';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    redirect: { name: 'HomePage' },
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: 'home', name: 'HomePage', component: HomePage },
