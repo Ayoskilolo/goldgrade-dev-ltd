4d92f8c0547c596b84fcb73985e46271bd098e54
Daramola Oluwadunsin <daramoladunsin6@gmail.com>
2023-09-08 20:41:08 +0100

    Initial commit

--- src/pages/ClientsPage.vue
+++ src/pages/ClientsPage.vue
@@ -1,14 +1,239 @@
 <template>
   <q-page class="section">
-    <section class="hero-text">
-      <p>We're here to improve you.</p>
-      <p></p>
-    </section>
+    <div class="space">
+      <div class="cont">
+        <section class="hero-text">
+          <h4>OUR CLIENTS</h4>
+          <p>
+            At Goldgrade, we're proud of our diverse client base, trusting us to
+            boost their performance and success. We serve organizations of all
+            sizes, adapting our expertise to their unique needs. We stand out
+            with a comprehensive approach to performance improvement, focusing
+            on organizational, team, and individual levels. Our seasoned experts
+            collaborate closely with clients, delivering practical solutions in
+            strategy, implementation, processes, and skill development.
+          </p>
+          <p>
+            Choose Goldgrade for a dedicated partner in your journey to success.
+            Join our satisfied clients who've witnessed remarkable
+            transformations. Together, we'll elevate your performance and secure
+            a prosperous future.
+          </p>
+          <p>Some of our clients include:</p>
+        </section>
+        <section class="customer-grid">
+          <div class="grid">
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/swipha-customer.jpg"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>swipha</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :ratio="16 / 9"
+                :no-spinner="true"
+                src="/fmn-customer.png"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>FMN</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/polaris-customer.png"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>Polaris Bank</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/county-customer.jpg"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>County Choice Farms</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/kimberly-customer.png"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>Kimberly Clark</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/lirs-customer.jpg"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>Lagos IRS</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/harvestfield-customer.png"
+                img-class="cus-img"
+                fit="contain"
+                style="width: 100%; height: auto; max-height: 150px"
+              />
+              <p>Harvestfield Industries</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+            <div class="customer">
+              <q-img
+                :no-spinner="true"
+                src="/saro-customer.png"
+                img-class="cus-img"
+                fit="contain"
+                style="
+                  width: 100%;
+                  height: auto;
+                  max-height: 150px;
+                  background-color: #fff;
+                "
+              />
+              <p>Saro Lifecare</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente aspernatur asperiores sunt, tempore neque labore
+                nesciunt quas facilis rem reprehenderit similique nostrum
+                repudiandae temporibus impedit nam, mollitia deleniti ut
+                inventore. Enim error asperiores, aspernatur quisquam voluptas
+                at recusandae autem distinctio earum et nam laboriosam
+                architecto tenetur dolores veniam dicta quae.
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <style>
 .hero-text {
-  padding: 2rem 8%;
+  padding: 2%;
+  color: #c19f62;
+}
+.space {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.cont {
+  /* padding: 5rem; */
+  width: 90%;
+  /* background-color: green; */
+}
+
+.customer-grid {
+  padding: 0%;
+  margin-bottom: 10%;
+}
+
+.customer-grid > .grid {
+  width: 100%;
+  /* background-color: pink; */
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 20px;
+  row-gap: 15px;
+}
+
+.grid > .customer {
+  min-height: 300px;
+  color: #c19f62;
+  border: 1px solid #c19f62;
+  /* background-color: #fff; */
+}
+.customer > p {
+  padding: 4%;
+}
+/* .cus-img {
+  border-bottom: 1px solid #c19f62;
+} */
+
+.customer:hover {
+  color: #681c26;
+  background-color: #fff;
+  border: 2px solid #c19f62;
 }
 </style>
