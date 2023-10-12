194ab09e876e87e626b7774f4e753d7416088f62
Ayomide Afolabi <ayoafo82@gmail.com>
2023-10-12 23:13:06 +0100

    Edits on All Pages to finish UI work.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,7 +8,6 @@
         <q-route-tab :to="{ name: 'ContactUsPage' }" label="CONTACT US" />
         <q-route-tab label="BLOG" />
       </q-tabs>
-      <q-toolbar></q-toolbar>
     </q-header>
 
     <q-page-container>
@@ -16,7 +15,11 @@
     </q-page-container>
 
     <q-footer class="footer row">
-      <div class="col-3 icon"></div>
+      <div class="col-3 icon items-center">
+        <q-img src="/goldgrade.png" spinner-color="white" width="100px" />
+        <span> Gold<span style="color: #c19f62">Grade</span></span>
+        <span>Development Limited</span>
+      </div>
       <div class="col-9 footer-left">
         <div class="row">
           <div class="col-6">
@@ -41,14 +44,20 @@
 
             <div class="q-pt-lg">
               <p>
-                E-mail <br />
-                enquires@goldgradedevelopment.com
+                E-mail: <br />
+                <a
+                  href="mailto:enquires@goldgradedevelopment.com"
+                  style="text-decoration: none; color: white"
+                >
+                  enquires@goldgradedevelopment.com
+                </a>
               </p>
             </div>
           </div>
         </div>
-        <div class="row"></div>
-        <div class="row"></div>
+        <div class="row cc" style="font-size: calc(10px + 0.390625vw)">
+          <p>©{{ year }} GoldGrade Development Group.</p>
+        </div>
       </div>
     </q-footer>
   </q-layout>
@@ -56,11 +65,14 @@
 
 <script setup lang="ts">
 const footerLinkItems = ['Home', 'About Us', 'Clients', 'Contact Us', 'Blog'];
+
+const d = new Date();
+const year = d.getFullYear();
 </script>
 
 <style>
 .header {
-  background-color: transparent;
+  background-color: #681c26;
   font-family: Neurial;
 }
 
@@ -71,9 +83,10 @@ const footerLinkItems = ['Home', 'About Us', 'Clients', 'Contact Us', 'Blog'];
 }
 
 .footer {
-  height: 90vh;
+  height: 80vh;
   border-top: #c19f62 1px solid;
   background-color: #681c26;
+  font-family: NeurialLight;
 }
 
 .footer-left {
@@ -82,10 +95,29 @@ const footerLinkItems = ['Home', 'About Us', 'Clients', 'Contact Us', 'Blog'];
 
 .icon {
   border-right: 1px #c19f62 solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  padding-top: 6rem;
+}
+
+.icon span {
+  font-size: calc(14px + 0.390625vw);
 }
 
 .footer-text {
   font-size: calc(16px + 0.390625vw);
+  cursor: pointer;
+}
+
+.cc {
+  position: absolute;
+  bottom: 0;
+  padding: 4rem;
+  padding-left: 0;
+  width: 60%;
+  border-top: 1px solid #c19f62;
 }
 
 .link:hover {
@@ -98,5 +130,11 @@ body {
 
 p {
   font-family: NeurialLight;
+}
+
+input::placeholder {
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: red;
+  opacity: 1; /* Firefox */
 }
 </style>
--- src/pages/AboutUsPage.vue
+++ src/pages/AboutUsPage.vue
@@ -1,41 +1,103 @@
 <template>
-  <q-page class="section">
+  <q-page class="aboutus section">
+    <section class="hero"><p>Who We Are</p></section>
     <section>
-      <p>Who We Are</p>
-
       <div>
         Goldgrade Development Limited is a performance improvement advisory
         company focused on enhancing performance at organisational, team and
-        individual levels.
+        individual levels. We work with our clients to determine precisely where
+        and how to prime and leverage their organizations for major improvements
+        in market share, revenue and costs optimisation.
       </div>
     </section>
     <section>
-      <div>
-        Our aim is to improve overall business performance by examining and
-        providing practical solutions on strategy and implementation, processes
-        and procedures, as well as assist our clients in the development of
-        knowledge, skills and competences critical to their success. We work
-        with our clients to determine precisely where and how to prime and
-        leverage their organizations for major improvements in market share,
-        revenue and costs optimisation.
-      </div>
+      <div></div>
 
-      <div class="row items-center">
-        <p>Our Aim</p>
+      <div class="row items-center item">
+        <p class="col"><b>Our Aim</b></p>
+        <p class="col">
+          Our aim is to improve overall business performance by examining and
+          providing practical solutions and intervention on strategy and
+          implementation, processes and procedures, knowledge, skills and
+          competences in our area of focus.
+        </p>
       </div>
-      <div class="row items-center">
-        <p>Our Vision</p>
-        <p></p>
+      <div class="row items-center item">
+        <p class="col"><b>Our Vision</b></p>
+        <p class="col">
+          To grow the quality and competence of business managers.
+        </p>
       </div>
-      <div class="row items-center">
-        <p>Our mission</p>
+      <div class="row items-center item">
+        <p class="col"><b>Our Mission</b></p>
+        <p class="col">
+          To improve overall business performance of our client through a team
+          of dedicated associates and partners using the best of processes. in
+          the most sustainable way, and enjoying ourselves while doing this.
+        </p>
+      </div>
+    </section>
+    <section class="director row">
+      <div class="col-8"></div>
+      <div class="col-4" style="text-align: center">
+        <q-img src="/director.jpg" spinner-color="white" style="width: 250px" />
+
+        <p
+          style="
+            font-family: NeurialRegular;
+            font-size: calc(15px + 0.390625vw);
+            margin: 0;
+          "
+        >
+          Tokunbo Adegboye
+        </p>
+        <span>Managing Director</span>
       </div>
     </section>
   </q-page>
 </template>
 
-<style>
-section {
+<style scoped>
+.aboutus {
+  background-color: white;
+  color: black;
+}
+
+.section {
   padding: 2rem 8%;
+  font-size: 20px;
+  color: black;
+}
+
+.hero {
+  padding: 0;
+  display: flex;
+  height: 20vh;
+  color: black;
+  background-color: white;
+  align-items: center;
+  font-size: calc(70px + 0.390625vw);
+}
+
+.hero p {
+  font-family: Neurial;
+}
+
+.item {
+  border-bottom: 1px solid #c19f62;
+  height: 20vh;
+  padding: 1rem 3%;
+}
+
+.director {
+  display: flex;
+  margin-top: 4rem;
+  justify-content: flex-end;
+  padding: 2rem 8%;
+}
+
+.director span {
+  font-size: calc(10px + 0.390625vw);
+  font-family: NeurialLight;
 }
 </style>
